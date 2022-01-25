17d568767a7d0db6d1637d0a0bfd125fa264f2bf
Sharon Padippurackal <2558994P@student.gla.ac.uk>
2022-01-25 11:17:15 +0000

    Chapter 5 + Exercise

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
+
+    def view(self):
+        return self.views
+
+    def like(self):
+        return self.likes
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
 # Create your models here.
